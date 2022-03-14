695d330da9c6885373b4632b1583ec2fbe90ee0c
Pham Van Tuan <phamvantuan2304@gmail.com>
2022-03-14 15:30:55 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+'use strict';
+
+const express = require('express');
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+
+app.get('/', (req, res) => res.send('Welcome!'));
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
